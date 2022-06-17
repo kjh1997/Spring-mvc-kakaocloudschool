87bafcc4610da943a48da802ab5d1f97f36830a2
kjh <aam41@naver.com>
2022-06-17 13:45:57 +0900

    configuration

--- core/src/main/java/hello/core/member/MemberServiceImpl.java
+++ core/src/main/java/hello/core/member/MemberServiceImpl.java
@@ -19,4 +19,10 @@ public class MemberServiceImpl implements MemberService{
     public Member findMember(Long memberId){
         return memberRepository.findById(memberId);
     }
+
+
+    // test
+    public MemberRepository getMemberRepository(){
+        return memberRepository;
+    }
 }
