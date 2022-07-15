ffd457f84cc02190d1d0eaad00914b5efa105082
kjh <aam41@naver.com>
2022-07-15 12:20:15 +0900

    new prj

---
+++ 0715/src/main/java/com/kjh5/Application.java
@@ -0,0 +1,13 @@
+package com.kjh5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
---
+++ 0715/src/main/java/com/kjh5/domain/Account.java
@@ -0,0 +1,43 @@
+package com.kjh5.domain;
+
+import lombok.*;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Setter
+@EqualsAndHashCode(of = "id")
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class Account {
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    @Column(unique = true)
+    private String email;
+    @Column(unique = true)
+    private String nickname;
+    private boolean emailVerified;
+    private LocalDateTime joinedAt;
+    private String emailCheckToken;
+    private String bio;
+    private String url;
+    private String occupation;
+    private String location;
+    private String profileImage;
+
+    private boolean studyCreatedByEmail;
+    private boolean studyCreateByWeb;
+    private boolean studyEnrollmentResultByEmail;
+    private boolean studyEnrollmentResultByWeb;
+    private boolean studyUpdateByWeb;
+
+
+}
