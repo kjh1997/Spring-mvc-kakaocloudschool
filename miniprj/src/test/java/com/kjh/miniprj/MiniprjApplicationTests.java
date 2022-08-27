9b5b20ae84c892d847b437a64bcceab9a2dd2145
kjh <aam41@naver.com>
2022-08-27 12:04:19 +0900

    mini

---
+++ miniprj/src/test/java/com/kjh/miniprj/MiniprjApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kjh.miniprj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiniprjApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
